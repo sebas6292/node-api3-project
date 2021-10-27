addef5b09a010998451b694870ac83fc96d902b1
Sebastian <sebas6292@gmail.com>
2021-10-27 15:18:37 -0700

    timestamp created successfully with logger

--- api/server.js
+++ api/server.js
@@ -1,16 +1,14 @@
 const express = require('express');
-
+const { logger } = require('./middleware/middleware.js')
 const server = express();
 
-// remember express by default cannot parse JSON in request bodies
 server.use(express.json())
+
+server.use(logger)
 // global middlewares and the user's router need to be connected here
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
-server.get('*', (req, res) => { 
-  res.status(404).json({ message: `${req.method} ${req.baseUrl} not found!`})
-})
 module.exports = server;
